6b0a06b14568058394058150b48e92cf71e397d8
harishfuego33 <harishkumar.ct20@bitsathy.ac.in>
2024-06-05 16:55:06 +0530

    responsive layout completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ const App = () => {
   const [year, setYear] = useState("");
   const [cvc, setCvc] = useState("");
   const [alert, setalert] = useState(false);
+  const [cardHolderName, setCardHolderName] = useState("");
+  const [active, setActive] = useState(false);
   let tempCard = "0000 0000 0000 0000";
   let tempMM = "00";
   let tempYY = "00";
   let tempCVC = "000";
+  let tempName = "Jane Appleseed";
   useEffect(() => {
     for (let i = 0; i < card.length; i++) {
       if (isNaN(card[i])) setalert(true);
@@ -21,47 +24,44 @@ const App = () => {
       setalert(false);
     };
   }, [alert, card]);
+  if (card.length) tempName = cardHolderName;
   if (card.length === 19) tempCard = card;
   if (year.length === 2) tempYY = year;
-  if (month.length === 2) tempMM = month;
+  if (month.length === 2 || month.length === 1) tempMM = month;
   if (cvc.length === 3) tempCVC = cvc;
   return (
     <main className="card__main">
       <section className="card__section--1">
         <div className="card__img--1">
-          <img
-            src="/images/bg-card-front.png"
-            alt="cardimg"
-            className="card__img-bg-1"
-          />
           <div className="card__log--box">
-            <img src="/images/card-logo.svg" alt="card__log" />
+            <img
+              src="/images/card-logo.svg"
+              alt="card__log"
+              className="card__log"
+            />
           </div>
           <p className="card__number">{tempCard}</p>
           <div className="card__holder">
-            <p className="card__holder--name">JASIKA JELYBRAD</p>
+            <p className="card__holder--name">{tempName}</p>
             <span className="card__holder-exp">
               {tempMM}/{tempYY}
             </span>
           </div>
         </div>
         <div className="card__img--2">
-          <img
-            src="/images/bg-card-back.png"
-            alt="cardimg"
-            className="card__img-bg-1"
-          />
           <p className="card__back--cvc">{tempCVC}</p>
         </div>
       </section>
-      <section className="card__section--2">
-        <form action="#" className="card__form">
+      <section className={`card__section--2 ${active ? "hidden" : ""}`}>
+        <div className="card__form">
           <div className="card__form--name">
             <p className="card__form--placeholdername">CARDHOLDER NAME</p>
             <input
               type="text"
               className="card__form-input"
               placeholder="e.g. Jane Appleseed"
+              value={cardHolderName}
+              onChange={(e) => setCardHolderName(e.target.value)}
               name="name"
               required
             />
@@ -98,11 +98,11 @@ const App = () => {
                 </p>
                 <input
                   type="number"
-                  className="card__input--date"
+                  className="card__input--date-1"
                   placeholder="MM"
                   name="cardnumber"
                   value={month}
-                  id="expdate"
+                  id="expmon"
                   min="1"
                   max="12"
                   onChange={(e) => setMonth(e.target.value)}
@@ -110,17 +110,20 @@ const App = () => {
                 />
                 <input
                   type="number"
-                  className="card__input--date"
+                  className="card__input--date-2"
                   placeholder="YY"
                   name="cardnumber"
-                  id="expdate"
+                  id="expyear"
                   value={year}
                   min="1"
                   max="99"
                   onChange={(e) => setYear(e.target.value)}
                   required
                 />
-                <label htmlFor="expdate" className="date-inputlabel">
+                <label htmlFor="expmon" className="date-inputlabel-1">
+                  Cant&apos;be blank
+                </label>
+                <label htmlFor="expyear" className="date-inputlabel-2">
                   Cant&apos;be blank
                 </label>
               </div>
@@ -144,8 +147,22 @@ const App = () => {
               </label>
             </div>
           </div>
-          <button className="card__btn">Confirm</button>
-        </form>
+          <button className="card__btn" onClick={() => setActive(!active)}>
+            Confirm
+          </button>
+        </div>
+      </section>
+      <section className={`card__section--2 ${active ? "" : "hidden"}`}>
+        <div className="card__form align-items-center">
+          <div className="thank__you--logo">
+            <img src="/images/icon-complete.svg" alt="thick-mark" />
+          </div>
+          <h1 className="thank__you">thank you!</h1>
+          <p className="acknowledgement">We&apos;ve added your card details</p>
+          <button className="card__btn" onClick={() => setActive(!active)}>
+            Continue
+          </button>
+        </div>
       </section>
     </main>
   );
